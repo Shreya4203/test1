965028c295c912fb00d209f2f37386951945a8a6
Shreya4203 <shreyarajpoot7788@gmail.com>
2024-01-18 13:17:01 +0530

    yo

--- hungry.py
+++ hungry.py
@@ -1,7 +1,6 @@
 hungry = input("are you hungry")
-if hungry=="yes":
+if hungry=="yes" and hungry=="yo":
     print("eat samosa")
-    print("eat pizza")
     print("eat burger")
 else:
     thirsty = input("are you thirsty")
